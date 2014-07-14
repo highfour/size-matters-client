0ffd350ab24977c44928e7e8cccf88d788cfdb53
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-07-14 18:01:48 +0200

    leeres libgdx android/desktop projekt

---
+++ desktop/src/com/highfour/size-matters-client/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.highfour.size-matters-client.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.highfour.size-matters-client.SMClient;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SMClient(), config);
+	}
+}
