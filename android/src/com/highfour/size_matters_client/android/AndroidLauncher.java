11ca477e200828316c13489051dec3c33d99e1ce
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-07-14 23:14:58 +0200

    ordentlicher whitespace

--- android/src/com/highfour/size_matters_client/android/AndroidLauncher.java
+++ android/src/com/highfour/size_matters_client/android/AndroidLauncher.java
@@ -7,10 +7,10 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.highfour.size_matters_client.SMClient;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SMClient(), config);
-	}
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new SMClient(), config);
+    }
 }
